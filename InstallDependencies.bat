0b71e009dcc0f4788cbbc627554d4bfd4eecc26e
kevinkovalchik <kevin.kovalchik@gmail.com>
2018-03-21 13:36:57 -0700

    Added package versions to pip commands

--- InstallDependencies.bat
+++ InstallDependencies.bat
@@ -1,10 +1,11 @@
 echo off
 title Installing RawQuant dependencies
 
-pip install numpy
-pip install pandas
-pip install tqdm
+pip install numpy==1.14.2
+pip install pandas==0.22.0
+pip install tqdm==4.19.8
 pip install comtypes==1.1.3
+pip install joblib==0.11
 
 echo Done installing dependencies. If any installations failed, please refer to that package's documentation.
 pause
