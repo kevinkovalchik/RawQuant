cc7ba57495795ec248a79e070c6bc9c312e73458
Kevin Kovalchik <kkovalchik@bcgsc.ca>
2018-03-23 11:50:51 -0700

    update version number, dev status, email

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from distutils.core import setup
 setup(
     name='RawQuant',
     packages=['RawQuant'],
-    version='0.1.1',
+    version='0.1.2',
     description='Package for extracting scan meta data and quantification information from Thermo .raw files',
     long_description='RawQuant is a Python package for extracting scan meta data and quantification values from ' +
                      'Thermo .raw files.',
     author='Kevin Kovalchik',
-    author_email='kevin.kovalchik@gmail.com',
+    author_email='kkovalchik@bcgsc.ca',
     url='https://github.com/kevinkovalchik/RawQuant',
     project_urls={'Documentation': 'https://github.com/kevinkovalchik/RawQuant/docs'},
     keywords=['Orbitrap', 'mass spectrometer', 'isobaric labelling', 'quantification', 'scan meta data'],
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta',
         'Operating System :: Microsoft :: Windows',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
