771fa4c71ba4763f4c8cd90eae0c7d268d4b03ec
Kevin Kovalchik <kevin.kovalchik@gmail.com>
2018-04-30 19:56:47 -0700

    update version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='RawQuant',
     packages=['RawQuant'],
-    version='0.1.4.1',
+    version='0.1.4.2',
     description='Package for extracting scan meta data and quantification information from Thermo .raw files',
     long_description='RawQuant is a Python package for extracting scan meta data and quantification values from ' +
                      'Thermo .raw files.',
